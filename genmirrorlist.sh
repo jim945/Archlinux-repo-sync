da26ea0d1bdf5bc84da5494da15229c790cbb1af
jim945 <jim945@mail.ru>
2021-09-09 20:44:22 +0300

    fix error in genmirrorlist.sh

--- genmirrorlist.sh
+++ genmirrorlist.sh
@@ -3,4 +3,4 @@
 # jim945@mail.ru 
 
 workdir=`dirname "$(readlink -e "$0")"`
-reflector -a 1 --sort rate --completion-percent 100 -p rsync -n 5 > $workdir/mirrorlist
+reflector -a 1 --completion-percent 100 -p rsync -n 5 > $workdir/mirrorlist
